e8506a18773d9c0c4ab98b3dddcb55d4175ff148
Veronica <vgalbicka@gmail.com>
2024-05-03 15:08:37 -0300

    agrega metodo en clase Categoria
    transforma String a un tipo de dato de nuestra Clase personalizada que usamos para atributo

--- src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -12,4 +12,22 @@ public enum Categoria {
     Categoria (String categoriaOmdb){
         this.categoriaOmdb = categoriaOmdb;
     }
+
+    /*
+    *  estamos haciendo la transformación de string a
+    * un tipo de dato de nuestra categoría.
+    * traemos esos valores  coincide con las variables que tenemos aquí ya mapeadas,
+    *  ahí va a hacer esa transformación.
+    *  Y nos va a retornar a categoría del tipo.
+    *  En caso de que no pueda,  ninguna categoría fue encontrada*/
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+
 }
