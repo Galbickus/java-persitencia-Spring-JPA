041e0e99bf0ca6694a8df870ca50ade010cc22c6
Veronica <vgalbicka@gmail.com>
2024-05-03 15:07:01 -0300

    Completa clase Serie ver constructor IMPORTANTEy agrega metodo en clase Categoria

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -1,5 +1,7 @@
 package com.aluracursos.screenmatch.model;
 
+import java.util.OptionalDouble;
+
 public class Serie {
 
     private String titulo;
@@ -10,5 +12,80 @@ public class Serie {
     private String actores;
     private String sinopsis;
 
+    public Serie(DatosSerie datosSerie) {
+        this.titulo = datosSerie.titulo();
+        this.totalTemporadas = datosSerie.totalTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0);
+        this.poster = datosSerie.poster();
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
+        this.actores = datosSerie.actores();
+        this.sinopsis = datosSerie.sinopsis();
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + titulo + '\'' +
+                ", Total de temporadas: " + totalTemporadas +
+                ", Evaluacion: " + evaluacion +
+                ", Poster: " + poster + '\'' +
+                ", Genero: " + genero +
+                ", Actores: " + actores + '\'' +
+                ", Sinopsis: " + sinopsis + ". ";
+    }
 }
