91e210f3bc711085d4af524f6b30f3d1187c8cc1
Veronica <vgalbicka@gmail.com>
2024-05-03 02:00:55 -0300

    add class Serie

---
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -0,0 +1,14 @@
+package com.aluracursos.screenmatch.model;
+
+public class Serie {
+
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double evaluacion;
+    private String poster;
+    private Categoria genero;
+    private String actores;
+    private String sinopsis;
+
+
+}
