93e98126d6cc593d25e61b4e70f7aa8da49b672e
Veronica <vgalbicka@gmail.com>
2024-05-05 20:51:32 -0300

    Agrega constructor vacio
    Es una exigencia de JPA que todas nuestras entidades tengan un constructor vacio (o constructos que no reciba parámetros)

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -23,6 +23,9 @@ public class Serie {
     @Transient
     private List<Episodio> episodios;
 
+    public Serie() {
+    }
+
     public Serie(DatosSerie datosSerie) {
         this.titulo = datosSerie.titulo();
         this.totalTemporadas = datosSerie.totalTemporadas();
