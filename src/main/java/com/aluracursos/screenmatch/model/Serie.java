63d8ce5665065df508eba9737759260216b0dcb9
Veronica <vgalbicka@gmail.com>
2024-05-05 22:39:53 -0300

    Add atrib Serie  relacionar episodio con su
    agrego el atributo serie de tipo Serie para relacionar cada episodio con su serie, agrego get y set del nuevo atributo y constructos vacio para persistir

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -20,7 +20,7 @@ public class Serie {
     private Categoria genero;
     private String actores;
     private String sinopsis;
-
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios;
 
     public Serie() {
