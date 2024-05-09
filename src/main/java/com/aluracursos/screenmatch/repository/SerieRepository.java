03cea8f75e4ec3c2e76c17eea27f143f82f4663e
Veronica <vgalbicka@gmail.com>
2024-05-09 20:55:12 -0300

    @query

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,13 @@ public interface SerieRepository extends JpaRepository<Serie,Long> {
     List<Serie> findTop5ByOrderByEvaluacionDesc();
 
     List<Serie> findByGenero(Categoria categoria);
-    List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas, Double evaluacion);
 
+    //List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas, Double evaluacion);
+ /*   @Query(value = "SELECT * FROM series WHERE series.total_temporadas <=6 \n" +
+            "AND series.evaluacion>=7.5", nativeQuery = true)*/
+
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas \n" +
+            "AND s.evaluacion >= :evaluacion")
+
+    List<Serie> seriesPorTemporadaYEvaluacion(int totalTemporadas, Double evaluacion);
 }
