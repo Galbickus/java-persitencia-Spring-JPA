afb185458ed5e4180fb68dcf7146aae488441d94
Veronica <vgalbicka@gmail.com>
2024-05-15 19:49:46 -0300

    Update SerieController.java
    vamos a crear otro getMapping. l ID. Recordando que, como el ID es dinámico, lo vamos a trabajar con llaves. Colocamos aquí ID, cierro la llave, barra temporadas, barra todas. Es esa la URL que voy a querer mapear, esa es la ruta. Y aquí, en esta oportunidad, voy a querer devolver no una serie sino una lista de episodios, recordando que las temporadas están compuestas por los episodios.

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.EpisodioDTO;
 import com.aluracursos.screenmatch.dto.SerieDTO;
 import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
@@ -42,6 +43,10 @@ public class SerieController{
         @GetMapping("/{id}")
     public SerieDTO obtenerPorId(@PathVariable Long id){
         return servicio.obtenerPorId(id);
+    }
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obtenerTodasLasTemporadas(@PathVariable Long id) {
+        return servicio.obtenerTodasLasTemporadas(id);
     }
 
 }
