c5ef18e18115f337ffcfd9cae458763136c448b0
Veronica <vgalbicka@gmail.com>
2024-05-15 21:53:54 -0300

    Update SerieRepository.java
    AGREGA METODO OBTENER TEMPORADAS POR NUMERO

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch.repository;
 
+import com.aluracursos.screenmatch.dto.EpisodioDTO;
 import com.aluracursos.screenmatch.model.Categoria;
 import com.aluracursos.screenmatch.model.Episodio;
 import com.aluracursos.screenmatch.model.Serie;
@@ -19,6 +20,7 @@ public interface SerieRepository extends JpaRepository<Serie,Long> {
 
     @Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.evaluacion >= :evaluacion")
     List<Serie> seriesPorTemporadaYEvaluacion(int totalTemporadas, double evaluacion);
+
     // List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas, Double evaluacion);
     @Query("SELECT s FROM Serie s " + "JOIN s.episodios e " + "GROUP BY s " + "ORDER BY MAX(e.fechaDeLanzamiento) DESC LIMIT 5")
     List<Serie> lanzamientosMasRecientes();
@@ -42,8 +44,14 @@ public interface SerieRepository extends JpaRepository<Serie,Long> {
     @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas \n" +
             "AND s.evaluacion >= :evaluacion")
     List<Serie> seriesPorTemporadaYEvaluacion(int totalTemporadas, Double evaluacion);
+
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:nombreEpisodio%")
     List<Episodio> episodiosPorNombre(String nombreEpisodio);
+
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.evaluacion DESC LIMIT 5")
     List<Episodio> top5Episodios(Serie serie);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numeroTemporada")
+    List<Episodio> obtenerTemporadasPorNumero(Long id, Long numeroTemporada);
+
 }
