9e49a5d0193cd2cbd82821a1bbf5721ae2217749
Veronica <vgalbicka@gmail.com>
2024-05-15 21:58:11 -0300

    Mapeamos categoria
    para que aaparezcan en la busqueda por cate. nombramos el metodo y vamos a crearlo en el servicio

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -55,4 +55,9 @@ public class SerieController{
         return servicio.obtenerTemporadasPorNumero(id,numeroTemporada);
     }
 
+    @GetMapping("/categoria/{nombreGenero}")
+    public List<SerieDTO> obtenerSeriesPorCategoria(@PathVariable String nombreGenero){
+        return servicio.obtenerSeriesPorCategoria(nombreGenero);
+    }
+
 }
